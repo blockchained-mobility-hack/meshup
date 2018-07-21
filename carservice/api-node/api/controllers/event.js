365b0ab697f3f0597eef11525109f7ee5535cab1
Isabel Povoa <misabelpovoa@gmail.com>
2018-07-21 20:56:34 +0200

    added ;

--- carservice/api-node/api/controllers/event.js
+++ carservice/api-node/api/controllers/event.js
@@ -77,15 +77,15 @@ function postevent(req, res) {
       }
     })*/
     //const trytes = 'HELLOXXXXXHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDD'
-    const vehicle1 = 'VEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEV'
-    const vehicle2 = 'VEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOV'
-    const vehicle3 = 'VEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRV'
-    const vehicle4 = 'VEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUV'
+    const vehicle1 = 'VEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEVEHICLEONEV';
+    const vehicle2 = 'VEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOVEHICLETWOV';
+    const vehicle3 = 'VEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRVEHICLETHRV';
+    const vehicle4 = 'VEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUVEHICLEFOUV';
     //const message = iota.utils.toTrytes('Airbag: OMG, I just released!')
-    sendMessage (vehicle1,40.717000, -74.006400, 'Airbag: OMG, I just released!')
-    sendMessage (vehicle2,40.717000, -74.006400, 'Breaks: Nailed it!! From 100 to 0 in 3s')
-    sendMessage (vehicle3,40.717000, -74.006400, 'Cruise Control: Why is everyone slowing down')
-    sendMessage (vehicle4,40.717000, -74.006400, 'Meshup: Detected Accident ahead')
+    sendMessage (vehicle1,40.717000, -74.006400, 'Airbag: OMG, I just released!');
+    sendMessage (vehicle2,40.717000, -74.006400, 'Breaks: Nailed it!! From 100 to 0 in 3s');
+    sendMessage (vehicle3,40.717000, -74.006400, 'Cruise Control: Why is everyone slowing down');
+    sendMessage (vehicle4,40.717000, -74.006400, 'Meshup: Detected Accident ahead');
 
     //console.log(JSON.stringify(req.swagger.params.body));
     //state = req.swagger.params.body.value.event || "seems well";
@@ -95,16 +95,16 @@ function postevent(req, res) {
 * Sends a message from a vehicle together with the message to the tangle
 */
 function sendMessage(vehicleAddress, lat, long, message) {
-  const messageTryte = iota.utils.toTrytes(message)
-  var roundedLat = parseFloat(lat).toFixed(2)
-  var roundedLong = parseFloat(long).toFixed(2)
-  var tag = roundedLat.concat(roundedLong)//.replace(".", "D").replace(".", "D").replace("-", "M")
+  const messageTryte = iota.utils.toTrytes(message);
+  var roundedLat = parseFloat(lat).toFixed(2);
+  var roundedLong = parseFloat(long).toFixed(2);
+  var tag = roundedLat.concat(roundedLong); //.replace(".", "D").replace(".", "D").replace("-", "M")
   var transactionMessage = '{ "message":' + message + ', "latitude":' + lat + ', "longitude":' + long + ' }';
-  console.log('tag')
-  console.log(tag)
-  console.log(iota.utils.toTrytes(tag))
-  console.log('message')
-  console.log(transactionMessage)
+  console.log('tag');
+  console.log(tag);
+  console.log(iota.utils.toTrytes(tag));
+  console.log('message');
+  console.log(transactionMessage);
   const transfers = [
   {
       value: 0,
@@ -112,12 +112,12 @@ function sendMessage(vehicleAddress, lat, long, message) {
       message: iota.utils.toTrytes(transactionMessage),
       tag: iota.utils.toTrytes(tag)
     }
-  ]
+  ];
   iota.api.sendTransfer(vehicleAddress, 3, 9, transfers, (error, success) => {
     if (error) {
       console.log(error)
     } else {
       console.log(success)
     }
-  })
+  });
 }
